2deb6b6ad7d3fef49fbde48ab6317a387b4836ea
”Nick <lilmodou14@gmail.com>
2024-06-24 01:56:24 +0300

    first

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -1,6 +1,7 @@
 package koschei;
 
 import koschei.config.AppConfig;
+import koschei.models.Ocean1;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,5 +12,6 @@ public class Application {
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
         System.out.println(koscheiTheDeathless.getRulesByDeth());
+        
     }
 }
